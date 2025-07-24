acd6ae46da19c2c7690412d7a1a5d7ed976eb1eb
Coursemite <coursemite@gmail.com>
2025-07-24 23:15:37 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,20 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "1.9.23"
+    }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = System.getenv("ProductName") ?: "Suwayomi-Server"
 
 include("server")
 include("server:i18n")
-
 include("AndroidCompat")
 include("AndroidCompat:Config")
 
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
